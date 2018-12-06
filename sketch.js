5b46fb95b7d88ab75400cde6fbeee763579139c0
shreyanshkotak <shreyansh.kotak@somaiya.edu>
2018-12-06 14:37:36 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var style;
+var video;
+var isTransferring = false;
+var resultImg;
+
+function setup() {
+  createCanvas(320, 240).parent('canvasContainer');
+
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // The results image from the style transfer
+  resultImg = createImg('');
+  resultImg.hide();
+
+  // The button to start and stop the transfer process
+  select('#startStop').mousePressed(startStop);
+
+  // Create a new Style Transfer method with a defined style.
+  // We give the video as the second argument
+  style = ml5.styleTransfer('models/rain_princess', video, modelLoaded);
+}
+
+function draw(){
+  // Switch between showing the raw camera or the style
+  if (isTransferring) {
+    image(resultImg, 0, 0, 320, 240);
+  } else {
+    image(video, 0, 0, 320, 240);
+  }
+}
+
+// A function to call when the model has been loaded.
+function modelLoaded() {
+  select('#status').html('Model Loaded');
+}
+
+// Start and stop the transfer process
+function startStop() {
+  if (isTransferring) {
+    select('#startStop').html('Start');
+  } else {
+    select('#startStop').html('Stop');
+    // Make a transfer using the video
+    style.transfer(gotResult); 
+  }
+  isTransferring = !isTransferring;
+}
+
+// When we get the results, update the result image src
+function gotResult(err, img) {
+  resultImg.attribute('src', img.src);
+  if (isTransferring) {
+    style.transfer(gotResult); 
+  }
+}
